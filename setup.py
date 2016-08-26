37abc37cbf7cdf7d12772b3fa2de3a59828c9e5d
Andy Dai <daikeren@gmail.com>
2016-08-26 17:15:51 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from distutils.core import setup
 
-from pip.req import parse_requirements
-from pip.download import PipSession
-
-install_reqs = parse_requirements('requirements.txt', session=PipSession())
-
-reqs = [str(ir.req) for ir in install_reqs]
-
 setup(
     name='pybotbuilder',
-    version='0.0.1',
+    version='0.0.2',
     packages=['botbuilder', ],
     author='Andy Dai',
-    install_requires=reqs,
+    install_requires=['requests', ],
+    url='https://github.com/daikeren/pyBotBuilder',
     description='Python interface for Microsoft BotFramework',
     author_email='daikeren@gmail.com',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    long_description='Python interface for Microsoft BotFramework',
 )
